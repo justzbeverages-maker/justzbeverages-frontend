bbb1fa7dd585fece3949c9c3c1f730aecd42720f
Ronit Solanki <ronitbsolanki@gmail.com>
2026-09-26 01:48:16 +0530

    fixed logo on contact us page

--- src/Components/HeaderOther.jsx
+++ src/Components/HeaderOther.jsx
@@ -3,7 +3,7 @@ import {NavLink} from 'react-router'
 export function HeaderOther({openMenu}){
     return(
          <div className="navBar color">
-        <div className="logo color"><NavLink to="/" className="navlink">JUSTZ</NavLink></div>
+        <div className="logo color"><NavLink to="/" className="navlinkother">JUSTZ</NavLink></div>
         <div className="nav-cen color"></div>
         <div className="nav color" onClick={openMenu} >
             <i className="ri-menu-line"></i>
